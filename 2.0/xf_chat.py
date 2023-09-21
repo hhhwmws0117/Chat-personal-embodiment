0d1429b9458f3b87af4c1072b0496b08aa11f7be
chenxi <to_do_chenxi@163.com>
2023-09-21 23:49:12 +0800

    delete key

--- 2.0/xf_chat.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from chatharuhi import ChatHaruhi
-import openai
-
-openai.api_key = "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"
-chatbot = ChatHaruhi.chat("hello")
-print(chatbot)
