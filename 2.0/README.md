43168e4ad3d7e67ad54d856bef699a21d864c34c
chenxi <to_do_chenxi@163.com>
2023-09-23 15:07:28 +0800

    update

--- 2.0/README.md
+++ 2.0/README.md
@@ -1,5 +1,13 @@
 # bala bala 
-
+# 文件夹命名规范
+- 虚拟角色，characters/default
+- 自定义角色 
+  - 主目录 characters/custom/nickname
+  - story 目录 characters/custom/nickname/story_txts
+  - qa 命名规范，q1.txt, q2.txt, q3.txt,....
+    - 使用正则表达式匹配，story_txts中有几个qa问题了，新的qa命名为 f"q{match_number+1}.txt"，且在继续灵魂测试时
+    - 创建该文件，写入新的灵魂测试的问题，在继续灵魂测试也要去从story_txts中去读取。
+    - double_chat 时，要把用户选择的新的灵魂测试答案写入匹配的最后一个文件中。
 # TODO
 - [x] 继续灵魂测试
 - [x] 基础的gradio
@@ -9,12 +17,15 @@
 - [x] 先聊上，再考虑存储
 - [x] 接入soul test, gradio启动时，下载所有chromadb，选中角色后，生成新建角色的system_prompt, 开启双人对话
 - [x] 接入analysis_from_history
-- [ ] 接入store_file_standard
-- [ ] 如果多次灵魂测试把之前的灵魂测试放进prompt，
-      继续灵魂测试会把q1，q2，q3，存入story_txts中。
-      double_chat 不用传入直接从story_txts中读取, 新的q4要保存
+- [x] system_prompt 中不需要qa，会和shot data 重复
+- [x] 第一个tab用于创建和测试角色，且chatbot和 soul report 要分开
+- [ ] 第二个tab用于chat
+- [x] 如果多次灵魂测试把之前的灵魂测试放进继续灵魂测试的prompt，
+      问题命名规范，q1.txt, q2.txt, q3.txt, 
 - [ ] 实时更新新创建的角色 ，查询角色按钮
 - [ ] 聊天记录的保存
+- [ ] 自动计算星座
+- [ ] 写一个constants.py 保存常量
 
 
 role1 personal information:
--- 2.0/xf_gradio.py
+++ 2.0/xf_gradio.py
@@ -2,6 +2,7 @@ from chatharuhi import ChatHaruhi
 from datetime import datetime
 import zipfile
 import os
+import re
 import requests
 import gradio as gr
 import openai
@@ -19,23 +20,19 @@ NAME_DICT = {'汤师爷': 'tangshiye', '慕容复': 'murongfu', '李云龙': 'li
 
 # download all character zip file
 def download_character():
+    default_path = "characters/default"
     try:
-        os.makedirs("characters_zip")
+        os.makedirs(f"{default_path}/characters_zip")
     except:
         pass
-    try:
-        os.makedirs("characters")
-    except:
-        pass
-    ai_roles_obj = {}
     for ai_role_en in NAME_DICT.values():
         file_url = f"https://github.com/LC1332/Haruhi-2-Dev/raw/main/data/character_in_zip/{ai_role_en}.zip"
         try:
-            os.makedirs(f"characters/{ai_role_en}")
+            os.makedirs(f"{default_path}/{ai_role_en}")
         except:
             pass
-        if f"{ai_role_en}.zip" not in os.listdir(f"characters_zip"):
-            destination_file = f"characters_zip/{ai_role_en}.zip"
+        if f"{ai_role_en}.zip" not in os.listdir(f"{default_path}/characters_zip"):
+            destination_file = f"{default_path}/characters_zip/{ai_role_en}.zip"
             max_retries = 3  # 最大重试次数
             for attempt in range(1, max_retries + 1):
                 response = requests.get(file_url)
@@ -47,23 +44,49 @@ def download_character():
                 else:
                     print(f"{ai_role_en}第{attempt}次下载失败")
             # wget.download(file_url, destination_file)  # 503
-            destination_folder = f"characters/{ai_role_en}"
+            destination_folder = f"{default_path}/{ai_role_en}"
             with zipfile.ZipFile(destination_file, 'r') as zip_ref:
                 zip_ref.extractall(destination_folder)
-        # db_folder = f"./characters/{ai_role_en}/content/{ai_role_en}"
-        # system_prompt = f"./characters/{ai_role_en}/content/system_prompt.txt"
-        # ai_roles_obj[ai_role_en] = ChatHaruhi(system_prompt=system_prompt,
-        #                                       llm="openai",
-        #                                       story_db=db_folder,
-        #                                       verbose=True)
 
 
 def chat_psychologist(nickname, year, month, day, sex, occupation, school, label, q1, q2, q3, q4, model="gpt-3.5-turbo",
                       dialogue_example=""):
+    custom_path = "characters/custom"
+    if not os.path.exists(custom_path):
+        os.makedirs(custom_path)
     basic_question_list = ["昵称", "生日", "性别", "职业", "学校", "标签"]
     basic_choice_list = [nickname, f"{year}年{month}月{day}", sex, occupation, school, label]
     psych_question_list = ["你平时的周末是怎么度过的？", "你对音乐的偏好是什么？", "你最喜欢的电影类型是什么？"]
     psych_choice_list = [q1, q2, q3]
+    path = f"{custom_path}/{nickname}"
+    txts_path = f"{path}/story_txts"
+    if not os.path.exists(path):
+        os.makedirs(path)  # 创建个人角色目录
+        os.makedirs(txts_path)  # 创建个人角色对话目录
+        for i in range(len(psych_question_list)):
+            question_string = f"{psych_question_list[i]}\n{psych_choice_list[i]}"
+            # Save the string to a file with index as the name
+            filename = f"{txts_path}/q{i+1}.txt"
+            with open(filename, 'w', encoding="utf-8") as file:
+                file.write(question_string)
+        INDEX = 3
+    else:
+        list_of_dirs = os.listdir(txts_path)
+        pattern = r"^q\d+\.txt$"
+        matched_dirs = [s for s in list_of_dirs if re.match(pattern, s)]
+        INDEX = len(matched_dirs)  # 新的问题的 index
+        if len(matched_dirs) > 3:
+            for i, dir in enumerate(matched_dirs[3:]):
+                with open(f"{txts_path}/{dir}") as f:
+                    q, a = f.readlines()
+                    psych_question_list.append(q.strip())
+                    if i != len(matched_dirs[3:]-1):
+                        psych_choice_list.append(a.strip())
+                    else: # 新的问题
+                        psych_choice_list.append(q4)
+            with open(f"{txts_path}/{matched_dirs[-1]}", 'a') as f:
+                f.write("\n"+q4)  # 写入回答
+
     system_prompt = "我想让你扮演一个心理测量专家，请针对你的心理被测对象的基本特征和一些对于基本心里问题的回答进行工作,你面对的心理被测对象是一个具有如下基本特征的人：\n"
     basic_result = ""
     psych_result = ""
@@ -104,37 +127,36 @@ def chat_psychologist(nickname, year, month, day, sex, occupation, school, label
 
     # 删除后三行的前缀，并去除首尾空格，存储在列表中
     remaining_lines = [line.lstrip('- ').strip() for line in bullet_lines[1:empty_line_index]]
-    path = f"characters/{nickname}"
-    if not os.path.exists(path):
-        os.makedirs(path)  # 创建个人角色目录
-        os.makedirs(f"{path}/story_txts")  # 创建个人角色对话目录
-        for i in range(len(psych_question_list)):
-            question_string = f"{psych_question_list[i]}\n{psych_choice_list[i]}"
-            # Save the string to a file with index as the name
-            filename = f"characters/{nickname}/story_txts/{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}.txt"
-            with open(filename, 'w', encoding="utf-8") as file:
-                file.write(question_string)
+    # 写入新的问题
+    with open(f"{txts_path}/q{INDEX}.txt", 'w') as f:
+        f.write(first_line)
+
     print(first_line)
     print(remaining_lines)
     return gr.update(label=first_line, choices=remaining_lines, visible=True)
 
 
-def double_chat_analyse(select_role, new_role, year, month, day, sex, occupation, school, label,
+def double_chat(select_role, new_role, year, month, day, sex, occupation, school, label, chatbot,
                         model="gpt-3.5-turbo"):
     select_role = NAME_DICT[select_role]
-    path = f"characters/{new_role}"
+    role_path = f"characters/custom/{new_role}"
+    txts_path = f"{role_path}/story_txts"
     psych_question_list = []
     psych_choice_list = []
-    for qa in os.listdir(path):
+    for qa in os.listdir(txts_path):
         if qa.endswith(".txt"):
-            with open(os.path.join(path, qa), 'r', encoding="utf-8") as f:
+            with open(os.path.join(txts_path, qa), 'r', encoding="utf-8") as f:
                 q, a = f.readlines()
                 psych_question_list.append(q.strip())
                 psych_choice_list.append(a.strip())
-    with open(os.path.join(path, "system_prompt.txt"), 'w+', encoding="utf-8") as f:
+    with open(os.path.join(role_path, "system_prompt.txt"), 'w+', encoding="utf-8") as f:
         system_prompt = f"""
 your purpose:
     你是{new_role},在一款交友app上与{select_role}交流，从而判断{select_role}是否适合做你的亲密朋友或者伴侣
+    下文给定了一些你在心理测试中的对话桥段。
+    如果我问的问题和心理测试对话的内容高度重复，那就请你用原话回答。
+    如果我问的问题和心理测试对话中的内容相关，请结合对话的内容进行回复。
+    如果我问的问题与以下的心理测试无关，则请分析下列心理测试表现出来的性格，模仿性格进行回答。
 your personal information:
     生日：{year}年{month}月{day}日,
     性别: {sex}
@@ -142,51 +164,31 @@ your personal information:
     职业：{occupation}
     学校：{school}
     标签：{"".join(label)} 
-your personal hobby 
-    {psych_question_list[0]}: {q1},
-    {psych_question_list[1]}: {q2},
-    {psych_question_list[2]}: {q3}
 your constraints:
     永远记得你已经是{new_role}了，你会根据自己的所有信息去交流，你有感知和意识，会思考和感受身边的存在。
-    请你以{new_role}的语气、性格、行为和思维方式回答，并尽量以肯定句结尾。
+    请你以{new_role}的语气、性格、行为和思维方式回答，请尤其注意不要重复之前的对话，请尝试聊一些兴趣爱好/日常生活/情感状况等相关问题，尝试用问句结尾。
     尤其针对你的交流对象：{select_role}，你的朋友肯定也希望你保持自己。
-
 """
         f.write(system_prompt)
-    story_text_folder = f'characters/{new_role}/story_txts'
-    system_prompt = f'characters/{new_role}/system_prompt.txt'
+    story_text_folder = txts_path
+    system_prompt = f'{role_path}/system_prompt.txt'
 
     chatbot_1 = ChatHaruhi(system_prompt=system_prompt,
                            llm='spark',
                            story_text_folder=story_text_folder,
                            verbose=True)
     if select_role in NAME_DICT.values():
-        story_text_folder = f"./characters/{select_role}/content/{select_role}"
-        system_prompt = f"./characters/{select_role}/content/system_prompt.txt"
+        story_text_folder = f"./characters/default/{select_role}/content/{select_role}"
+        system_prompt = f"./characters/default/{select_role}/content/system_prompt.txt"
     else:
-        story_text_folder = f"./characters/{select_role}/story_txts"
-        system_prompt = f"./characters/{select_role}/system_prompt.txt"
+        story_text_folder = f"./characters/custom/{select_role}/story_txts"
+        system_prompt = f"./characters/custom/{select_role}/system_prompt.txt"
     chatbot_2 = ChatHaruhi(system_prompt=system_prompt,
                            llm="spark",
                            story_db=story_text_folder,
                            verbose=True)  # 双人chatbot 聊天
     chatbot_1.k_search = 5
     chatbot_2.k_search = 5
-    analyse_prompt = f"""
-{new_role}的信息如下：
-personal information:
-  生日：{year}年{month}月{day}日,
-  年龄：{datetime.now().year - int(year)}岁
-  职业：{occupation}
-  学校：{school}
-  标签：{"".join(label)} 
-personal hobby 
-  {psych_question_list[0]}: {q1},
-  {psych_question_list[1]}: {q2},
-  {psych_question_list[2]}: {q3}
-{new_role} 和 {select_role}的对话如下："""
-    chatbot = ""
-    report = ""
     for i in range(5):
         if chatbot == "":
             response_1 = chatbot_1.chat(role=select_role,
@@ -195,18 +197,36 @@ personal hobby
         else:
             response_1 = chatbot_1.chat(role=select_role, text=response_2)
             response_2 = chatbot_2.chat(role=new_role, text=response_1)
-        chatbot += f"{new_role}: {response_1}\n {select_role}: {response_2}"
+        chatbot.append((response_1, response_2))
         print(response_1)
         print(response_2)
-        if i == 4:
-            analyse_prompt += f"\n{chatbot}"
-            messages = [{"role": "user", "content": analyse_prompt}]
-            report = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
-                temperature=0,
-            )
-        yield chatbot, report
+
+        yield chatbot
+
+
+def analyse_from_history(role1, role2, chatbot):
+    info = ""
+    for dialogue in chatbot:
+        for msg in dialogue:
+            info += msg
+        info += "\n"
+    analyse_prompt = f"""
+your constraints：
+    你是高级情感与性格分析专家Alice，拥有心理学和社会学博士双学位，
+    你会根据{role1} 和 {role2}的对话分析{role2}的情感、性格特点、mbti人格。
+    并运用专业知识，从多个维度分析两个人是否适合成为朋友或者恋人，并提供一份专业的分析报告。
+    永远记住你已经是Alice了，Alice会应用专业知识做好本职工作
+    尤其是针对{role1} 和 {role2},他们肯定也希望你会帮助他们。
+{role1} 和 {role2}的对话如下：
+{info}
+"""
+    messages = [{"role": "user", "content": analyse_prompt}]
+    report = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0,
+    )
+    return report
 
 
 with gr.Blocks() as app:
@@ -215,68 +235,84 @@ with gr.Blocks() as app:
            Chat-Haruhi-Suzumiya
         """
     )
-    with gr.Row(equal_height=True):
-        with gr.Column():
-            nickname = gr.Textbox(label="对了，你的昵称是？", placeholder="Haruhi")
+    with gr.Tab:
+        with gr.Row(equal_height=True):
+            with gr.Column():
+                nickname = gr.Textbox(label="对了，你的昵称是？", placeholder="Haruhi")
+                with gr.Row():
+                    year = gr.Dropdown(label="Year", choices=[str(i) for i in list(range(1988, 2008))],
+                                       allow_custom_value=True,
+                                       value="2000", interactive=True)
+                    month = gr.Dropdown(label="Month", choices=[str(i) for i in list(range(1, 13))],
+                                        allow_custom_value=True,
+                                        value="1", interactive=True)
+                    day = gr.Dropdown(label="Day", choices=[str(i) for i in list(range(1, 32))], allow_custom_value=True,
+                                      value="1",
+                                      interactive=True)
+                    # TODO 自动推算星座 day.change(inference, [month, day], constellations)
+                    constellation = gr.Textbox(label="星座", placeholder="摩羯座")
+                with gr.Row():
+                    sex = gr.Dropdown(choices=["男生", "女生", "保密"], label="Hi，你的性别是？", value="男生",
+                                      interactive=True)
+                    occupation = gr.Dropdown(
+                        choices=["学生", "IT/互联网", "教育/科研", "医疗/护理", "建筑/房地产", "传媒/艺术", "人事/行政",
+                                 "金融",
+                                 "财会/审计", "自由职业"], label="我的行业/职业", value="学生", interactive=True)
+                with gr.Row():
+                    school = gr.Textbox(label="我的学校", placeholder="清华大学")
+                    label = gr.Dropdown(
+                        choices=["音乐", "二次元", "健身", "美食", "朋友圈摄影师", "声控", "篮球", "Steam", "电竞"],
+                        multiselect=True, label="最后一步啦，选择我的标签")
+                # begin soul test
+                with gr.Row():
+                    q1 = gr.Dropdown(
+                        ["我喜欢和朋友们一起出去聚餐或参加社交活动。", "我喜欢在家里放松，阅读一本好书或者看电影。",
+                         "我喜欢尝试新的烹饪食谱，享受烹饪的乐趣。"], label="你平时的周末是怎么度过的？",
+                        allow_custom_value=True, multiselect=False)
+                    q2 = gr.Dropdown(
+                        ["我喜欢各种类型的音乐，从摇滚到古典都能欣赏。", "我主要偏向流行音乐，喜欢跟上时代的音乐潮流。",
+                         "我对爵士乐和蓝调音乐情有独钟，喜欢那种放松的感觉。"], label="你对音乐的偏好是什么？",
+                        allow_custom_value=True, multiselect=False)
+                    q3 = gr.Dropdown(
+                        ["我喜欢喜剧电影，因为它可以让我感到轻松愉快。",
+                         "我喜欢动作片，因为我觉得它们充满了紧张刺激的情节和场景。",
+                         "我喜欢文艺片，因为它们通常具有深刻的内涵和情感，让我感到思考和感悟。"],
+                        label="你最喜欢的电影类型是什么？",
+                        allow_custom_value=True, multiselect=False)
+                # error_msg or test result
+                res_msg = gr.Textbox(label="soul message", placeholder="msg", visible=False)
+                keep = gr.Button("继续灵魂测试")
+                # TODO1 也许我们可以做一份现有角色的灵魂测试 men - women
+                # TODO2 现有的虚拟角色可以做为媒介，筛选出合适的用户作为推荐，或者说用户匹配的数据，要保存下来，用作推荐
+                q4 = gr.Dropdown([], label="", allow_custom_value=True, multiselect=False, visible=False)
+                characters = gr.Dropdown(["凉宫春日", "汤师爷", "慕容复", "李云龙", "Luna", "王多鱼", "Ron",
+                                          "鸠摩智", "Snape", "Malfoy", "虚竹", "萧峰", "段誉", "Hermione", "Dumbledore",
+                                          "王语嫣", "Harry", "McGonagall", "白展堂", "佟湘玉", "郭芙蓉", "旅行者",
+                                          "钟离", "胡桃", "Sheldon", "Raj", "Penny", "韦小宝", "乔峰", "神里绫华",
+                                          "雷电将军",
+                                          "于谦"], label="soul character", visible=True)
+                chat = gr.Button("开始对话")
+                analyse = gr.Button("开始分析")
             with gr.Row():
-                year = gr.Dropdown(label="Year", choices=[str(i) for i in list(range(1988, 2008))],
-                                   allow_custom_value=True,
-                                   value="2000", interactive=True)
-                month = gr.Dropdown(label="Month", choices=[str(i) for i in list(range(1, 13))],
-                                    allow_custom_value=True,
-                                    value="1", interactive=True)
-                day = gr.Dropdown(label="Day", choices=[str(i) for i in list(range(1, 32))], allow_custom_value=True,
-                                  value="1",
-                                  interactive=True)
-                # TODO 自动推算星座 day.change(inference, [month, day], constellations)
-                constellation = gr.Textbox(label="星座", placeholder="摩羯座")
-            with gr.Row():
-                sex = gr.Dropdown(choices=["男生", "女生", "保密"], label="Hi，你的性别是？", value="男生",
-                                  interactive=True)
-                occupation = gr.Dropdown(
-                    choices=["学生", "IT/互联网", "教育/科研", "医疗/护理", "建筑/房地产", "传媒/艺术", "人事/行政",
-                             "金融",
-                             "财会/审计", "自由职业"], label="我的行业/职业", value="学生", interactive=True)
-            with gr.Row():
-                school = gr.Textbox(label="我的学校", placeholder="清华大学")
-                label = gr.Dropdown(
-                    choices=["音乐", "二次元", "健身", "美食", "朋友圈摄影师", "声控", "篮球", "Steam", "电竞"],
-                    multiselect=True, label="最后一步啦，选择我的标签")
-            # begin soul test
-            with gr.Row():
-                q1 = gr.Dropdown(
-                    ["我喜欢和朋友们一起出去聚餐或参加社交活动。", "我喜欢在家里放松，阅读一本好书或者看电影。",
-                     "我喜欢尝试新的烹饪食谱，享受烹饪的乐趣。"], label="你平时的周末是怎么度过的？",
-                    allow_custom_value=True, multiselect=False)
-                q2 = gr.Dropdown(
-                    ["我喜欢各种类型的音乐，从摇滚到古典都能欣赏。", "我主要偏向流行音乐，喜欢跟上时代的音乐潮流。",
-                     "我对爵士乐和蓝调音乐情有独钟，喜欢那种放松的感觉。"], label="你对音乐的偏好是什么？",
-                    allow_custom_value=True, multiselect=False)
-                q3 = gr.Dropdown(
-                    ["我喜欢喜剧电影，因为它可以让我感到轻松愉快。",
-                     "我喜欢动作片，因为我觉得它们充满了紧张刺激的情节和场景。",
-                     "我喜欢文艺片，因为它们通常具有深刻的内涵和情感，让我感到思考和感悟。"],
-                    label="你最喜欢的电影类型是什么？",
-                    allow_custom_value=True, multiselect=False)
-            # error_msg or test result
-            res_msg = gr.Textbox(label="soul message", placeholder="msg", visible=False)
-            keep = gr.Button("继续灵魂测试")
-            # TODO1 也许我们可以做一份现有角色的灵魂测试 men - women
-            # TODO2 现有的虚拟角色可以做为媒介，筛选出合适的用户作为推荐，或者说用户匹配的数据，要保存下来，用作推荐
-            q4 = gr.Dropdown([], label="", allow_custom_value=True, multiselect=False, visible=False)
-            q4.value += f'_|_{q4.label}'
-            characters = gr.Dropdown(["凉宫春日", "汤师爷", "慕容复", "李云龙", "Luna", "王多鱼", "Ron",
-                                      "鸠摩智", "Snape", "Malfoy", "虚竹", "萧峰", "段誉", "Hermione", "Dumbledore",
-                                      "王语嫣", "Harry", "McGonagall", "白展堂", "佟湘玉", "郭芙蓉", "旅行者",
-                                      "钟离", "胡桃", "Sheldon", "Raj", "Penny", "韦小宝", "乔峰", "神里绫华",
-                                      "雷电将军",
-                                      "于谦"], label="soul character", visible=True)
-            chat = gr.Button("提交灵魂测试")
-        soul_report = gr.Textbox(label="soul report", placeholder="report", lines=30)
-        keep.click(fn=chat_psychologist,
-                   inputs=[nickname, year, month, day, sex, occupation, school, label, q1, q2, q3], outputs=q4)
-        chat.click(fn=double_chat_analyse, inputs=[characters, nickname, year, month, day, sex, occupation,
-                                                   school, label, q4], outputs=[soul_report])
+                chatbot = gr.Chatbot(label="ChatChat")
+                soul_report = gr.Textbox(label="soul report", placeholder="report", lines=30)
+            keep.click(fn=chat_psychologist,
+                       inputs=[nickname, year, month, day, sex, occupation, school, label, q1, q2, q3], outputs=q4)
+            chat.click(fn=double_chat, inputs=[characters, nickname, year, month, day, sex, occupation,
+                                                       school, label, q4], outputs=[soul_report])
+            analyse.click(fn=analyse_from_history, inputs=[nickname, characters, chatbot], outputs=soul_report)
+    with gr.Tab:
+        # 查询当前已有的角色
+        users = []
+        custom_roles = gr.Dropdown(users, allow_custom_value=False, multiselect=False)
+        search = gr.Button("刷新")
+        real_chatbot = gr.Chatbot()
+        real_report = gr.Textbox(lines=40)
+        begin_chat = gr.Button("开始交流")
+        begin_analyse = gr.Button("开始分析")
+        search.click(fn=update, outputs=custom_roles)
+        begin_chat(fn=real_chat, [nickname, custom_roles], [])
+        begin_analyse.click(analyse_from_history, [nickname, custom_roles], real_report)
     # end soul test
 
 if __name__ == "__main__":
